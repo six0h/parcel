04d12ccc402a47ddbbd7486c0834f26c7d8f0241
Cody Halovich <cody@telenova.ca>
2012-10-28 13:36:54 -0700

    added created keys file

---
+++ admin/createkeys.php
@@ -0,0 +1,28 @@
+<?php
+
+$how_many_days = 120;
+
+require_once('../config.php');
+
+$key = rand_char(16);
+$hour = numberInRange(1,22,1);
+$minute = numberInRange(1,59,1);
+$second = numberInRange(1,59,1);
+$today = new MongoDate(strtotime('+ '.$hour[0].' hours '.$minute[0].' minutes '.$second[0].' seconds'));
+
+$insertToday = $db->insert('campaigns',
+	array(	'key' => $key,
+		'date' => $today));
+
+for($i = 1; $i < $how_many_days; $i++) {
+	$key = rand_char(16);
+	$hour = numberInRange(1,22,1);
+	$minute = numberInRange(1,59,1);
+	$second = numberInRange(1,59,1);
+	$random = strtotime("+ ".$i." days ".$hour[0]." hours ".$minute[0]." minutes ".$second[0]." seconds");
+	$insert = $db->insert('campaigns',
+		array(	'key' => $key,
+			'date' => new MongoDate($random)));
+}
+
+?>
